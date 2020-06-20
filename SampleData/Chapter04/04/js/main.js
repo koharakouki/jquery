2431349e94b7b1cff9ad5024ce1865e9488f518a
koharakouki <ko.kohara02@gmail.com>
2020-06-20 12:49:35 +0900

    [Fin]chapter4-4

--- SampleData/Chapter04/04/js/main.js
+++ SampleData/Chapter04/04/js/main.js
@@ -1,11 +1,28 @@
+// $(function(){
+//     //
+//     var duration = 300;
+
+//     // aside ----------------------------------------
+//     var $aside = $('.page-main > aside');
+//     var $asidButton = $aside.find('button')
+//         .on('click', function(){
+//             $aside.toggleClass('open');
+//             if($aside.hasClass('open')){
+//                 $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+//                 $asidButton.find('img').attr('src', 'img/btn_close.png');
+//             }else{
+//                 $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+//                 $asidButton.find('img').attr('src', 'img/btn_open.png');
+//             };
+//         });
+// });
+
+
 $(function(){
-    // 
     var duration = 300;
 
-    // aside ----------------------------------------
     var $aside = $('.page-main > aside');
-    var $asidButton = $aside.find('button')
-        .on('click', function(){
+    var $asidButton = $aside.find('button').on('click', function(){
             $aside.toggleClass('open');
             if($aside.hasClass('open')){
                 $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
@@ -15,5 +32,4 @@ $(function(){
                 $asidButton.find('img').attr('src', 'img/btn_open.png');
             };
         });
-
 });
