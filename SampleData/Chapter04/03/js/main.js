9717f44941aa3b683bafc9b2a4331d864cdcc8b4
koharakouki <ko.kohara02@gmail.com>
2020-06-20 09:43:35 +0900

    [Fin]chapter4-3

--- SampleData/Chapter04/03/js/main.js
+++ SampleData/Chapter04/03/js/main.js
@@ -1,10 +1,39 @@
+// $(function(){
+//     //
+//     var duration = 300;
+
+//     // buttons2 ----------------------------------------
+//     $('#buttons2 button').each(function(index){
+//         //var pos = Math.random() * 80 - 40;
+//         var pos = index * 40 - 40;
+//         $(this).css('top', pos);
+//     })
+//     .on('mouseover', function(){
+//         var $btn = $(this).stop(true).animate({
+//             backgroundColor: '#faee00',
+//             color: '#000'
+//         }, duration);
+//         $btn.find('img:first-child').stop(true).animate({opacity: 0}, duration);
+//         $btn.find('img:nth-child(2)').stop(true).animate({opacity: 1}, duration);
+//     })
+//     .on('mouseout', function(){
+//         var $btn = $(this).stop(true).animate({
+//             backgroundColor: '#fff',
+//             color: '#01b169',
+//         }, duration);
+//         $btn.find('img:first-child').stop(true).animate({opacity: 1}, duration);
+//         $btn.find('img:nth-child(2)').stop(true).animate({opacity: 0}, duration);
+//     });
+
+
+// });
+
+
 $(function(){
-    // 
+
     var duration = 300;
 
-    // buttons2 ----------------------------------------
     $('#buttons2 button').each(function(index){
-        //var pos = Math.random() * 80 - 40;
         var pos = index * 40 - 40;
         $(this).css('top', pos);
     })
@@ -13,17 +42,23 @@ $(function(){
             backgroundColor: '#faee00',
             color: '#000'
         }, duration);
-        $btn.find('img:first-child').stop(true).animate({opacity: 0}, duration);
-        $btn.find('img:nth-child(2)').stop(true).animate({opacity: 1}, duration);
+        $btn.find('img:first-child').stop(true).animate({
+            opacity: 0
+        }, duration);
+        $btn.find('img:nth-child(2)').stop(true).animate({
+            opacity: 1
+        }, duration);
     })
     .on('mouseout', function(){
         var $btn = $(this).stop(true).animate({
             backgroundColor: '#fff',
-            color: '#01b169',
+            color: '#01b169'
         }, duration);
-        $btn.find('img:first-child').stop(true).animate({opacity: 1}, duration);
-        $btn.find('img:nth-child(2)').stop(true).animate({opacity: 0}, duration);
+        $btn.find('img:first-child').stop(true).animate({
+            opacity: 1
+        }, duration);
+        $btn.find('img:nth-child(2)').stop(true).animate({
+            opacity: 0
+        }, duration);
     });
-
-
 });
