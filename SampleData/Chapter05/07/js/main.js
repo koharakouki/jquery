0b8239c693a498b87f284a43a9073565bb28f397
koharakouki <ko.kohara02@gmail.com>
2020-06-23 15:04:44 +0900

    [Fin]chapter5-7

--- SampleData/Chapter05/07/js/main.js
+++ SampleData/Chapter05/07/js/main.js
@@ -1,25 +1,57 @@
-$(function () {
+// $(function () {
 
-    /*
-     * Back-toTop button (Smooth scroll)
-     */
-    $('.back-to-top').each(function () {
+//     /*
+//      * Back-toTop button (Smooth scroll)
+//      */
+//     $('.back-to-top').each(function () {
 
-        // html か body のいずれがスクロール可能な要素かを検出
+//         // html か body のいずれがスクロール可能な要素かを検出
+//         var $el = $(scrollableElement('html', 'body'));
+
+//         // ボタンにクリックイベントを設定
+//         $(this).on('click', function (event) {
+//             event.preventDefault();
+//             $el.animate({ scrollTop: 0 }, 250);
+//         });
+//     });
+
+//     // scrollTop が利用できる要素を検出する関数
+//     // http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links#update4
+//     function scrollableElement (elements) {
+//         var i, len, el, $el, scrollable;
+//         for (i = 0, len = arguments.length; i < len; i++) {
+//             el = arguments[i],
+//             $el = $(el);
+//             if ($el.scrollTop() > 0) {
+//                 return el;
+//             } else {
+//                 $el.scrollTop(1);
+//                 scrollable = $el.scrollTop() > 0;
+//                 $el.scrollTop(0);
+//                 if (scrollable) {
+//                     return el;
+//                 }
+//             }
+//         }
+//         return [];
+//     }
+
+// });
+
+
+$(function(){
+    $('back-to-top').each(function(){
         var $el = $(scrollableElement('html', 'body'));
 
-        // ボタンにクリックイベントを設定
-        $(this).on('click', function (event) {
+        $(this).on('click', function(){
             event.preventDefault();
             $el.animate({ scrollTop: 0 }, 250);
         });
     });
 
-    // scrollTop が利用できる要素を検出する関数
-    // http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links#update4
-    function scrollableElement (elements) {
+    function scrollableElement(elements){
         var i, len, el, $el, scrollable;
-        for (i = 0, len = arguments.length; i < len; i++) {
+        for(i = 0, len = arguments.length; i < len; i++){
             el = arguments[i],
             $el = $(el);
             if ($el.scrollTop() > 0) {
@@ -35,5 +67,4 @@ $(function () {
         }
         return [];
     }
-
 });
