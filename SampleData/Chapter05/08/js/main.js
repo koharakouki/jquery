01beeaad9014b9ac3c16a5e036cc69febf2971f1
koharakouki <ko.kohara02@gmail.com>
2020-06-23 15:09:41 +0900

    [Fin]chapter5-8

--- SampleData/Chapter05/08/js/main.js
+++ SampleData/Chapter05/08/js/main.js
@@ -1,15 +1,24 @@
-$(function () {
+// $(function () {
 
-    /*
-     * Back-toTop button (Smooth scroll)
-     */
-    $('.back-to-top').on('click', function () {
+//     /*
+//      * Back-toTop button (Smooth scroll)
+//      */
+//     $('.back-to-top').on('click', function () {
 
-        // Smooth Scroll プラグインを実行
+//         // Smooth Scroll プラグインを実行
+//         $.smoothScroll({
+//             easing: 'easeOutExpo', // イージングの種類
+//             speed: 500             // 所要時間
+//         });
+//     });
+
+// });
+
+$(function(){
+    $('.back-to-top').on('click', function(){
         $.smoothScroll({
-            easing: 'easeOutExpo', // イージングの種類
-            speed: 500             // 所要時間
+            easing: 'easeOutExpo',
+            speed: 500
         });
     });
-
 });
