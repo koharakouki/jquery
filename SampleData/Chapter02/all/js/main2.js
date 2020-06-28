043a6a9713ce99cb11007cfb392de1bfb2b7ef2b
koharakouki <ko.kohara02@gmail.com>
2020-06-28 23:44:09 +0900

    [Fin]chapter2-3

--- SampleData/Chapter02/all/js/main2.js
+++ SampleData/Chapter02/all/js/main2.js
@@ -1,15 +1,15 @@
 
-$(function(){
-	$('#typo').css('color', '#ebc000');
-});
+// $(function(){
+// 	$('#typo').css('color', '#ebc000');
+// });
 
-$(function() {
-	$('#typo .inner').css('transform', 'rotate(10deg)');
-});
+// $(function() {
+// 	$('#typo .inner').css('transform', 'rotate(10deg)');
+// });
 
-$(function() {
-	$('#typo .inner').css('opacity', 0.5);
-});
+// $(function() {
+// 	$('#typo .inner').css('opacity', 0.5);
+// });
 
 // $()は、「HTML要素を元にしてjQuery独自の要素を生成する」という機能を持っている関数
 // →jQueryオブジェクト化するということ(jQueryの形式に変換された要素)
@@ -19,10 +19,20 @@ $(function() {
 // は、ブラウザがHTMLを最後まで読み込むのを待つための仕組み
 // 中に記述された処理をいったん予約状態にしておく
 
+// $(function() {
+// 	$('#typo').css({
+// 		fontSize: '50px',
+// 		backgroundColor: "#ae5e9b",
+// 		color: "#ebc000"
+// 	})
+// });
+
 $(function() {
-	$('#typo').css({
-		fontSize: '50px',
-		backgroundColor: "#ae5e9b",
-		color: "#ebc000"
+	$('#typo .inner')
+	.on('mouseover', function() {
+		$(this).css('color', '#000');
 	})
+	.on('mouseout', function() {
+		$(this).css('color', 'green');
+	});
 });
